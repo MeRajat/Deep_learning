7707120532850697ff09d666baf52823f2c52849
Rajat gupta <MeRajat@github.com>
2017-10-23 11:28:48 +0530

    updated for test
    Signed-off-by: Rajat gupta <MeRajat@github.com>

--- psuedo_label/dataset/dataset.py
+++ psuedo_label/dataset/dataset.py
@@ -10,7 +10,7 @@ from torch.autograd import Variable
 
 
 class data_loader(Dataset):
-    def __init__(self, path, transform=False, file_suffix = "*.jpg", label = None):
+    def __init__(self, path, transform=False, file_suffix = "*.jpg", label = None, test = False):
         super().__init__()
         self.path = path
         self.files = glob.glob(os.path.join(path ,"*.jpg"))
@@ -26,6 +26,8 @@ class data_loader(Dataset):
         img = cv2.resize(img, dsize = (200,200))
         img = np.einsum('ijk->kij', img)
         img = torch.from_numpy(img)
+        if test:
+            print(img_name)
 #         img = torch.FloatTensor(img)
         # d = {'image'  : img, 'name' : img_name, 'label' : self.label }
         label = os.path.basename(img_name).split('.')[0] == 'cat' if 0 else  1
